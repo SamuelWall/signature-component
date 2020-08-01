63dbb60aaf4d01fdee5b3135ccc1181c3e0c2cf1
Yitzchak Wolnerman <ywolnerman@gmail.com>
2020-07-31 20:08:13 -0400

    changed mobile buttons to look like web

--- src/components/SignaturePhone/index.js
+++ src/components/SignaturePhone/index.js
@@ -10,7 +10,7 @@ import Signature from 'react-native-signature-canvas';
 class SignaturePhone extends Component{
 	render = () => {console.log(this.props);
 		return (
-			<View style = {{width: this.props._width, height: this.props._height}}>
+			<View style = {{width: this.props._width+20, height: this.props._height}}>
 				<Signature
 				  // handle when you click save button
 				  onOK={(img) => {
@@ -27,17 +27,43 @@ class SignaturePhone extends Component{
 				  // String, webview style for overwrite default style, all style: https://github.com/YanYuanFE/react-native-signature-canvas/blob/master/h5/css/signature-pad.css
 				  webStyle={`.m-signature-pad {
 						  background-color: ${this.props.backgroundColor};
-							width: ${this.props._width};
-  						height: ${this.props._height};
+							width: ${this.props._width-2};
+  						height: ${this.props._height-2};
+              border: 1px solid black;
+              box-shadow: none;
 						}
-            .m-signature-pad--footer
-            {
-              height: 95px;
+            .m-signature-pad:before, .m-signature-pad:after {
+              left: 0px;
+              right: 0px;
+              box-shadow: none;
+            }
+            m-signature-pad:after {
+            	left: 0px;
+            	right: 0px;
+            }
+            .m-signature-pad--body{
+              bottom: 20px;
+            }
+            .m-signature-pad--body
+              canvas {
+                box-shadow: none;
+              }
+            .m-signature-pad--footer{
+              padding: 0px;
+              margin: 0px;
+              width: 100%;
+              left: 0px;
+              right: 0px;
             }
 						.m-signature-pad--footer
 							.button{
 								background-color: ${this.props.buttonBackgroundColor};
 								color: ${this.props.buttonTextColor};
+                width: 50%;
+                padding: 0px;
+                margin: 0px;
+                border: 1px solid black;
+                border-radius: 0px;
 							}
 						`
 				  }
